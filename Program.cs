16c6b2968c3192d3ba2ac2e0705d3860667e940c
Erismar Silva <erismar.silva@gbmlogistica.com.br>
2023-04-29 18:12:54 -0300

    Minimal APIs com SignalR

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.MapHub<MyHub>("/chat");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+class MyHub : Hub
+{
+    public async IAsyncEnumerable<DateTime> Streaming(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            yield return DateTime.UtcNow;
+            await Task.Delay(1000, cancellationToken);
+        }
+    }
+}
